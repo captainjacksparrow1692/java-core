50bb72e52dcd2290e9954455425effc56f432975
Jack Sparrow <captainjacksparrow12345piret@gmail.com>
2025-10-01 00:08:16 +0500

    Merge remote-tracking branch 'origin/main'

---
+++ lesson02/src/triangle/Main.java
@@ -0,0 +1,51 @@
+package triangle;
+
+public class Main {
+    public static void main(String[] args){
+        int n = 5;
+
+        // Прямой треугольник
+        for (int i=0; i<=n;i++){
+            for (int j=0; j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n");
+
+        // Обратный треугольник
+        for (int i=n; i>=1; i--){
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n");
+
+        // Зеркальный треугольник
+        for (int i=1; i<=n;i++){
+            for (int j=i; j<n;j++){
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n");
+
+        //Обратный треугольник
+        for (int i=n; i>=1; i--){
+            for (int j=i; j<=n; j++){
+                System.out.print(" ");
+            }
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
